049ec67176a3386cfee0f8f6a24753e50c4d73ab
Ben Do <bdo.appworkshop@gmail.com>
2024-06-02 19:27:14 -0500

    Imported NavigationContainer and createNativeStackNavigator.

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingPage from './components/landing-page/landing-page';
+import Login from './components/login/login';
+import SignUp from './components/sign-up/sign-up';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={LandingPage}
+          />
+          <Stack.Screen
+          name='Log In'
+          component={Login}
+          />
+          <Stack.Screen
+          name='Sign Up'
+          component={SignUp}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
