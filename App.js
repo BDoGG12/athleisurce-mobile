cb2bdf632e86049a2608a0f715424a4b27d9f371
Ben Do <bdo.appworkshop@gmail.com>
2024-06-05 23:48:10 -0500

    Edited import statements.

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingPage from './components/landing-page/landing-page';
-import Login from './components/login/login';
-import SignUp from './components/sign-up/sign-up';
+import LandingPage from './src/screens/landing-page/landing-page';
+import LogInPage from './src/screens/login-page/login-page';
+import SignUpPage from './src/screens/sign-up-page/sign-up-page';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,18 +14,18 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen
           name='Home'
           component={LandingPage}
           />
           <Stack.Screen
           name='Log In'
-          component={Login}
+          component={LogInPage}
           />
           <Stack.Screen
           name='Sign Up'
-          component={SignUp}
+          component={SignUpPage}
           />
       </Stack.Navigator>
     </NavigationContainer>
