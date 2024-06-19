69688d0b99ec3126ce612200319d5a4a84467b71
Ben Do <bdo.appworkshop@gmail.com>
2024-06-19 12:09:20 -0500

    Imported createBottomTabNavigator, FeaturedProducts, ShirtsPage, and PantsPage.

--- App.js
+++ App.js
@@ -3,31 +3,46 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import LandingPage from './src/screens/landing-page/landing-page';
+import FeaturedProducts from './src/screens/featured-products/featured-products';
 import LogInPage from './src/screens/login-page/login-page';
 import SignUpPage from './src/screens/sign-up-page/sign-up-page';
+import ShirtsPage from './src/screens/shirts-page/shirts-page.js';
+import PantsPage from './src/screens/pants-page/pants-page.js';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator initialRouteName='Home'>
+    <Stack.Screen
+      name='Featured Products'
+      component={FeaturedProducts}
+    />
+    <Stack.Screen
+      name='Log In'
+      component={LogInPage}
+    />
+    <Stack.Screen
+      name='Sign Up'
+      component={SignUpPage}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
 
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen
-          name='Home'
-          component={LandingPage}
-          />
-          <Stack.Screen
-          name='Log In'
-          component={LogInPage}
-          />
-          <Stack.Screen
-          name='Sign Up'
-          component={SignUpPage}
-          />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Shirts" component={ShirtsPage} />
+        <Tab.Screen name="Pants" component={PantsPage} />
+        <Tab.Screen name="Profile" component={HomeStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
