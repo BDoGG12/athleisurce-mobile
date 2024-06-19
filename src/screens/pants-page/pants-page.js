c3f63a7795d0fe8b21d8249d56a313b65589f63a
Ben Do <bdo.appworkshop@gmail.com>
2024-06-19 12:08:27 -0500

    Added PantsPage and ShirtsPage component.

---
+++ src/screens/pants-page/pants-page.js
@@ -0,0 +1,13 @@
+import {View, Text, Button} from 'react-native';
+import {useState, useEffect} from 'react';
+
+const PantsPage = () => {
+
+  return (
+    <View>
+      <Text>Pants Page</Text>
+    </View>
+  );
+};
+
+export default PantsPage;
---
+++ src/screens/shirts-page/shirts-page.js
@@ -0,0 +1,13 @@
+import {View, Text, Button} from 'react-native';
+import {useState, useEffect} from 'react';
+
+const ShirtsPage = () => {
+
+  return (
+    <View>
+      <Text>Shirts Page</Text>
+    </View>
+  );
+};
+
+export default ShirtsPage;
